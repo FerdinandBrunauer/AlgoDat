85a3fa1c8842c079d4322ab95c30c7e090778dea
Ferdinand Brunauer <ferdinand.brunauer@gmail.com>
2016-10-16 19:17:23 +0200

    Fixed Stack Added Queue Prototype

--- 2.8_StackQueueArrayImplementation/main.c
+++ 2.8_StackQueueArrayImplementation/main.c
@@ -8,11 +8,33 @@
 
 #pragma region ArrayStack
 
-typedef struct
+/*
+*	+---+
+*	| N |
+*	+---+
+*	| 5 |
+*	+---+
+*	| 4 |
+*	+---+
+*	| 3 |
+*	+---+
+*	| 2 |
+*	+---+
+*	| 1 |
+*	+---+
+*	| 0 |
+*	+---+
+*/
+typedef struct ArrayStack
 {
 	void* data[MAX_SIZE];
 	int top;
 } ArrayStack;
+
+void Init(ArrayStack* stack)
+{
+	stack->top = 0;
+}
 
 bool IsFull(ArrayStack* stack)
 {
@@ -26,6 +48,9 @@ bool IsEmpty(ArrayStack* stack)
 	return stack->top < 1;
 }
 
+/*
+* \return false if stack is full, true if it is inserted successful
+*/
 bool Push(ArrayStack* stack, void* data)
 {
 	if (IsFull(stack))
@@ -41,11 +66,11 @@ void* Pop(ArrayStack* stack)
 {
 	if (IsEmpty(stack))
 	{
-		return 0;
+		return NULL;
 	}
 
 	// It is voodoo magic. The -- decrements the value after executing the statement
-	return &stack->data[--stack->top];
+	return stack->data[--stack->top];
 }
 
 void* Top(ArrayStack* stack)
@@ -55,7 +80,42 @@ void* Top(ArrayStack* stack)
 		return 0;
 	}
 
-	return &stack->data[stack->top - 1];
+	return stack->data[stack->top - 1];
+}
+
+#pragma endregion
+
+#pragma region ArrayQueue
+
+/*
+*	FRONT					BACK
+*	+---+---+---+---+---+---+---+
+*	| 0 | 1 | 2 | 3 | 4 | 5 | N |
+*	+---+---+---+---+---+---+---+
+*/
+typedef struct ArrayQueue
+{
+	void* data[MAX_SIZE];
+} ArrayQueue;
+
+void* Front()
+{
+	return NULL;
+}
+
+void* Back()
+{
+	return NULL;
+}
+
+bool Enqueue(void* data)
+{
+	return false;
+}
+
+void* Dequeue(void* data)
+{
+	return NULL;
 }
 
 #pragma endregion
@@ -63,31 +123,40 @@ void* Top(ArrayStack* stack)
 int main()
 {
 	printf("** ArrayStack implementation **\n");
-	ArrayStack arrayStack = { 0 };
+	ArrayStack arrayStack;
+	// We have 2 possibilities. We init it with the method or use: ArrayStack arrayStack = { 0 };
+	Init(&arrayStack);
 	printf("Is full: %s\n", BOOL_TO_STRING(IsFull(&arrayStack)));
 	printf("Is empty: %s\n", BOOL_TO_STRING(IsEmpty(&arrayStack)));
-	printf("Add 11 elements: ");
-	for (int i = 65; i <= 75; ++i)
+
+	int* value = malloc(sizeof(int));
+	value = (int*)22;
+	printf("Push: %d->%s\n", (int)value, BOOL_TO_STRING(Push(&arrayStack, value)));
+	printf("Top: %d\n", (int)((int*)Top(&arrayStack)));
+	printf("Pop: %d\n", (int)((int*)Pop(&arrayStack)));
+
+	printf("Add 11 chars: ");
+	for (int i = 65; i < 76; ++i)
 	{
-		printf(i == 65 ? "%c->%s" : ", %c->%s", (char)i, BOOL_TO_STRING(Push(&arrayStack, (void*)i)));
+		void* value = malloc(sizeof(char));
+		value = (char*)i;
+		printf(i == 65 ? "%c->%s" : ", %c->%s", (char)i, BOOL_TO_STRING(Push(&arrayStack, value)));
 	}
 
 	printf("\nIs full: %s\n", BOOL_TO_STRING(IsFull(&arrayStack)));
 	printf("Is empty: %s\n", BOOL_TO_STRING(IsEmpty(&arrayStack)));
-	printf("Top: %s\n", (char*)Top(&arrayStack));
+	printf("Top: %c\n", (char)((char*)Top(&arrayStack)));
 
-	printf("\nPop 11 elements: ");
-	for (int i = 0; i <= 10; ++i)
+	printf("Pop 15 elements: ");
+	for (int i = 0; i < 15; ++i)
 	{
-		printf(i == 0 ? "%s" : ", %s", (char*)Pop(&arrayStack));
+		printf(i == 0 ? "%c" : ", %c", (char*)Pop(&arrayStack));
 	}
 
 	printf("\nIs full: %s\n", BOOL_TO_STRING(IsFull(&arrayStack)));
 	printf("Is empty: %s\n", BOOL_TO_STRING(IsEmpty(&arrayStack)));
 
-	char* test = "TestTestTestTest";
-	printf("Push: %s->%s\n", test, BOOL_TO_STRING(Push(&arrayStack, (void*)test)));
-	printf("Top: %s\n", (char*)Pop(&arrayStack));
+	Dispose(&arrayStack);
 
 	getchar();
 
